322da22a51c2ecf7858e7240574dfd819907da45
weiqlu <weiqlu@vt.edu>
2024-05-27 04:14:02 -0400

    fixing issues

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <NavBar />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/NavBar.css";
+
+function Header() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-logo">
+        EduGrade 
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
