3905a7dae6fa1e2d9f36ce84eb529d5b8e6e2b42
weiqlu <weiqlu@vt.edu>
2024-06-02 18:34:40 -0400

    modified package.json

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
+/* { origin: ["http://localhost:5000", "https://EduGrade.onrender.com"] } */
 app.use(cors());
 app.use(express.json());
 
