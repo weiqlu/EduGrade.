764ae271bfc29dbab38fcb9ce7ce3d6d74c9c1ea
weiqlu <weiqlu@vt.edu>
2024-06-04 12:16:30 -0400

    added port to the createConnection

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: 3306,
 });
 
 db.connect((err) => {
